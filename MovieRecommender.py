aa6cf41247d9f6e6f3955c948a907bee39bd653e
Bunny <59775180+git-gagan@users.noreply.github.com>
2021-10-12 01:02:30 +0530

    Movie recommender Script in Python

---
+++ MovieRecommender.py
@@ -0,0 +1,20 @@
+#Python Script for Movie Recommendation
+import requests
+import random
+
+def get_movie():
+    while True:  
+        id = random.randint(1000000,3999999)
+        data = requests.get(f"http://www.omdbapi.com/?i=tt{id}&apikey=#myprivatekey").json()
+        if data['Response'] == "True":
+            print(data)
+            if data["Year"] == "N\A" or "Episode" in data["Title"]:
+                continue
+            break
+    return data
+
+if __name__ == "__main__":
+    print(f"\nHey Visitor, Nice to meet ya! Welcome to Movie Recommender script!\n")
+    movie = get_movie()
+    print("|---------------------------Fetching your movie-----------------------------|\n")
+    print(f"I will recommend you to watch:- \"{movie['Title']}\" released in year:- {movie['Year']}\n")
